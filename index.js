576350eb1abd3e6c427069ce351f24ce3cf7c008
Rashmitha <rashmikulal831@gmail.com>
2023-09-10 11:49:17 +0530

    Updated UI

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ function updateClock() {
     const seconds = now.getSeconds().toString().padStart(2, 0);
     //Here 2 the digit it takes and the 0 is padding; if you use 3 and 0 it will give 005;
 
-    clock.textContent = now;
-    clock.textContent = hours;
-    clock.textContent = minutes;
-    clock.textContent = seconds;
 
     let timeString = '';
 
@@ -27,15 +23,17 @@ function updateClock() {
     }
     clock.textContent = timeString;
 }
+
 //Toggle 12 to 24
 document.getElementById('formatHours').addEventListener('click', function () {
     is24hour = !is24hour;
 
-    this.textContent = is24hour ? '12 Hours' : '24 Hours'
+    this.textContent = is24hour ? 'Click her for 24 Hours' : 'Click her for 12 Hours'
 
     updateClock();
 })
 
 setInterval(updateClock, 1000);
+//Here it will update eac 1sec
 
 updateClock();
